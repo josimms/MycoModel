83050859cfc6d3ff8abdf80cad9f8bad5d019a99
Joanna <joanna.x.simms@helsinki.fi>
2023-05-16 18:06:59 +0300

    Fungal uptake function added to the plots

--- R/overall_model.R
+++ R/overall_model.R
@@ -22,10 +22,12 @@ symphony_plus_vs_original_plot <- function() {
   symphony_plus_results = symphony_plus(symphony_parameters, Photosynthesis)
   
   par(mfrow = c(3, 2))
-  for (i in 1:length(symphony_plus_results)) { # TODO: what are these outputs?
+  names = c("Carbon Decomposers FOM", "Carbon Decomposers SOM", "Carbon Plant", "Carbon FOM", "Mineral N in Soil", "Carbon in SOM")
+  for (i in 1:length(symphony_plus_results)) {
     axis_min = min(symphony_plus_results[[i]], symphony_results[[i]])
     axis_max = max(symphony_plus_results[[i]], symphony_results[[i]])
     plot(symphony_plus_results[[i]], col = "blue", 
+         main = names[i],
          xlab = "Days", ylim = c(axis_min, axis_max), 
          ylab = names(symphony_plus_results)[i], type = "l")
     lines(symphony_results[[i]], col = "black")
@@ -37,12 +39,14 @@ nitrogen_graphs <- function() {
   # Indervidual functions should follow this pattern
   temp_change_organic <- temp_change_NO3 <- conc_change_organic <- conc_change_NO3 <- SWC_change_organic <- SWC_change_NO3 <- NULL
   temp_change_N_Uptake <- SWC_change_N_Uptake <- conc_change_N_Uptake <- conc_change_Norg_Uptake <- conc_change_NH4_Uptake <- conc_change_NO3_Uptake <- NULL
+  temp_change_N_Uptake_Fungal <- SWC_change_N_Uptake_Fungal <- conc_change_N_Uptake_Fungal <- conc_change_Norg_Uptake_Fungal <- conc_change_NO3_Uptake_Fungal <- conc_change_NH4_Uptake_Fungal <- NULL
   temperature_range = -20:20
   count = 1
   for (Temp in temperature_range) {
     temp_change_organic[count] = uptake_organic_N(10, Temp, 10, 0.2, 0.8, 0.5)
     temp_change_NO3[count] = uptake_NO3(10, Temp, 10, -0.2, 0.8, 0.5)
     temp_change_N_Uptake[count] = Plant_N_Uptake(c(0.5, 0.5, 0.5), c(10, 10, 10), 0.2, 15, Temp, 0.8, c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    temp_change_N_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, Temp, 0.8,c(10, 10, 10), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
     count = count + 1
   }
   concentration_range = seq(0, 20, by = 0.01)
@@ -54,6 +58,10 @@ nitrogen_graphs <- function() {
     conc_change_Norg_Uptake[count] = Plant_N_Uptake(c(0.5, 0.5, 0.5), c(conc, 10, 10), 0.2, 15, 15, 0.8, c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
     conc_change_NO3_Uptake[count] = Plant_N_Uptake(c(0.5, 0.5, 0.5), c(10, conc, 10), 0.2, 15, 15, 0.8, c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
     conc_change_NH4_Uptake[count] = Plant_N_Uptake(c(0.5, 0.5, 0.5), c(10, 10, conc), 0.2, 15, 15, 0.8, c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    conc_change_N_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, 15, 0.8, c(conc, conc, conc), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    conc_change_NO3_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, 15, 0.8, c(10, conc, 10), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    conc_change_NH4_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, 15, 0.8, c(conc, 10, 10), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    conc_change_Norg_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, 15, 0.8, c(10, 10, conc), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
     count = count + 1
   }
   SWC_range = seq(0, 1, by = 0.01)
@@ -62,8 +70,10 @@ nitrogen_graphs <- function() {
     SWC_change_organic[count] <- uptake_organic_N(10, 15, 10, 0.2, SWC, 0.5)
     SWC_change_NO3[count] <- uptake_NO3(10, 15, 10, -0.2, SWC, 0.5)
     SWC_change_N_Uptake[count] = Plant_N_Uptake(c(0.5, 0.5, 0.5), c(10, 10, 10), 0.2, 15, 15, SWC, c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
+    SWC_change_N_Uptake_Fungal[count] = Fungal_N_Uptake(200, 150, 200, 150, 500, 3000, 3000, 3000, 10, 15, SWC, c(10, 10, 10), c(10, 10, 10), c(0.2, -0.2, 0.2), c(0.5, 0.5, 0.5))
     count = count + 1
   }
+  
   par(mfrow = c(3, 2))
   plot(concentration_range, conc_change_organic, xlab = "Concentration", ylab = "Uptake of Organic / NH4", main = "Concentration response \n with temperature 15, SWC 0.8")
   plot(concentration_range, conc_change_NO3, xlab = "Concentration", ylab = "Uptake of NO3", main = "Concentration response \n with temperature 15, SWC 0.8")
@@ -80,8 +90,14 @@ nitrogen_graphs <- function() {
   plot(SWC_range, SWC_change_N_Uptake, xlab = "SWC", ylab = "total N to plant", main = "SWC response \n with concentation 10, temperature 15")
   plot(concentration_range, conc_change_Norg_Uptake, xlab = "Norg", ylab = "total N to plant", main = "Norg response \n with temperature 15, SWC 0.8, other N 10")
   
-  
+  par(mfrow = c(3, 2))
+  plot(concentration_range, conc_change_N_Uptake_Fungal, xlab = "Concentration", ylab = "total N to plant", main = "Concentration response \n with temperature 15, SWC 0.8")
+  plot(concentration_range, conc_change_NH4_Uptake_Fungal, xlab = "NH4", ylab = "total N to plant", main = "NH4 response \n with temperature 15, SWC 0.8, other N 10")
+  plot(temperature_range, temp_change_N_Uptake_Fungal, xlab = "Temperature", ylab = "total N to plant", main = "Temperature response \n with concentation 10, SWC 0.8")
+  plot(concentration_range, conc_change_NO3_Uptake_Fungal, xlab = "NO3", ylab = "total N to plant", main = "NO3 response \n with temperature 15, SWC 0.8, other N 10")
+  plot(SWC_range, SWC_change_N_Uptake_Fungal, xlab = "SWC", ylab = "total N to plant", main = "SWC response \n with concentation 10, temperature 15")
+  plot(concentration_range, conc_change_Norg_Uptake_Fungal, xlab = "Norg", ylab = "total N to plant", main = "Norg response \n with temperature 15, SWC 0.8, other N 10")
+
 }
 
-
-
+nitrogen_graphs()
