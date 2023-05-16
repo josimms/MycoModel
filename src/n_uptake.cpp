3b7dc18230b86f722c0189e882f852293f3bcc3a
Joanna <joanna.x.simms@helsinki.fi>
2023-05-16 18:07:23 +0300

    Fungal uptake model added

--- src/n_uptake.cpp
+++ src/n_uptake.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include <math.h>
 using namespace Rcpp;
 
 struct N_balence
@@ -48,6 +47,7 @@ double uptake_NH4(double NH4, double T, double NH4_limit, double k, double SWC, 
 
 // [[Rcpp::export]]
 double uptake_NO3(double NO3, double T, double NO3_limit, double k, double SWC, double SWC_k) {
+  // TODO: Uptake from the plant at least should depend on the lack of NH4 not the concentration of NH3 completely!
   double u = k * pow(NO3, 8) / (pow(NO3_limit, 8) + pow(NO3, 8)) - k;
   // Temperature
   double u_t = (T+20)/55;
@@ -56,7 +56,6 @@ double uptake_NO3(double NO3, double T, double NO3_limit, double k, double SWC, 
   double all = u * u_t * u_w;
   return(all);
 }
-
 
 // [[Rcpp::export]]
 double Plant_N_Uptake(std::vector<double> N_in_root_R, 
@@ -89,6 +88,45 @@ double Plant_N_Uptake(std::vector<double> N_in_root_R,
                                 uptake_NO3(N_in_soil.NO3, T, N_limits.NO3, N_k.NO3, SWC, SWC_k.NO3));
   return(N_to_root);
 }
+
+
+// [[Rcpp::export]]
+double Fungal_N_Uptake(double C_roots,
+                       double N_roots,
+                       double C_fungal,
+                       double N_fungal,
+                       double NC_fungal_opt,
+                       double mantle_mass,
+                       double ERM_mass,
+                       double N_avaliable,
+                       double N_to_CASSIA,
+                       double T,
+                       double SWC,
+                       std::vector<double> N_in_soil_R,
+                       std::vector<double> N_limits_R,
+                       std::vector<double> N_k_R,
+                       std::vector<double> SWC_k_R) {
+  
+  // TODO: make the N_available link to the uptake equations that I have made!
+  
+  N_balence N_in_soil = vector_to_N_balence(N_in_soil_R);
+  N_balence N_limits = vector_to_N_balence(N_limits_R);
+  N_balence N_k = vector_to_N_balence(N_k_R);
+  N_balence SWC_k = vector_to_N_balence(SWC_k_R);
+  
+  double NC_in_fungal = N_fungal/C_fungal;
+  // TODO: Parallel or intercecting?
+  double N_fungal_uptake = (uptake_organic_N(N_in_soil.Norg, T, N_limits.Norg, N_k.Norg, SWC, SWC_k.Norg) + 
+                            uptake_NH4(N_in_soil.Norg, T, N_limits.Norg, N_k.Norg, SWC, SWC_k.Norg) + 
+                            uptake_NO3(N_in_soil.Norg, T, N_limits.Norg, N_k.Norg, SWC, SWC_k.Norg))*
+                            (C_fungal/(C_roots))*(mantle_mass/ERM_mass)*N_avaliable*(1 - (NC_in_fungal)/(NC_fungal_opt));
+  
+  return(N_fungal_uptake);
+}
+
+
+
+
 
 
 
