941f1fbaa36e854968b51a4da78c79353db0903f
Joanna <joanna.x.simms@helsinki.fi>
2023-05-23 14:01:21 +0300

    package added in the preles call

--- R/preles_tests.R
+++ R/preles_tests.R
@@ -4,13 +4,13 @@ test_PRELES_N_function <- function() {
   nitrogen_range <- seq(0, 10, by = 1)
   count = 1
   for (n in nitrogen_range) {
-    output[[count]] <- PRELES(data_format$PAR, 
-                          data_format$T, 
-                          data_format$VPD, 
-                          data_format$Rain,
-                          data_format$CO2,
-                          data_format$fAPAR, 
-                          rep(n, length = nrow(data_format)))$GPP
+    output[[count]] <- Rprebasso::PRELES(data_format$PAR, 
+                                        data_format$T, 
+                                        data_format$VPD, 
+                                        data_format$Rain,
+                                        data_format$CO2,
+                                        data_format$fAPAR, 
+                                        rep(n, length = nrow(data_format)))$GPP
     count = count + 1 
   }
   
