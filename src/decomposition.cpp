51b0023acbeb0b7de8bbd821fb05acead2286de4
Joanna <joanna.x.simms@helsinki.fi>
2023-05-23 16:36:35 +0300

    Microbe decompositrion / uptake updated and therefore the decomposition file deleted. Functions went to n_uptake

--- src/decomposition.cpp
+++
@@ -1,8 +0,0 @@
-#include "mycomodel.h"
-
-// [[Rcpp::export]]
-double c_uptake_micobes(double C, double T, double C_limit, double k) {
-  // Aimed for the microbes
-  double u = k * power(C, 8) / (power(C_limit, 8) + power(C, 8));
-  return(u);
-}
--- src/n_uptake.cpp
+++ src/n_uptake.cpp
@@ -9,6 +9,7 @@ N_balence vector_to_N_balence(std::vector<double> input) {
   params.NH4 = input[0];
   params.NO3 = input[1];
   params.Norg = input[2];
+  params.C = input[3];
   
   return(params);
 }
@@ -43,6 +44,18 @@ double uptake_NH4(double NH4, double T, double NH4_limit, double k, double SWC, 
 double uptake_NO3(double NO3, double T, double NO3_limit, double k, double SWC, double SWC_k) {
   // TODO: Uptake from the plant at least should depend on the lack of NH4 not the concentration of NH3 completely!
   double u = k * pow(NO3, 8) / (pow(NO3_limit, 8) + pow(NO3, 8)) - k;
+  // Temperature
+  double u_t = (T+20)/55;
+  // Water
+  double u_w = SWC_k * pow(SWC, 8) / (pow(0.5, 8) + pow(SWC, 8));
+  double all = u * u_t * u_w;
+  return(all);
+}
+
+// [[Rcpp::export]]
+double uptake_C(double C, double T, double C_limit, double k, double SWC, double SWC_k) {
+  // TODO: Uptake from the plant at least should depend on the lack of NH4 not the concentration of NH3 completely!
+  double u = k * pow(C, 8) / (pow(C_limit, 8) + pow(C, 8)) - k;
   // Temperature
   double u_t = (T+20)/55;
   // Water
@@ -139,33 +152,46 @@ double Fungal_N_Uptake(double C_roots,
 
 
 // [[Rcpp::export]]
-double Microbe_N_Uptake(double C_microbe,
-                        double N_micorbe,
-                        double NC_microbe_opt,
-                        double N_avaliable,
-                        double T,
-                        double SWC,
-                        std::vector<double> N_in_soil_R,
-                        std::vector<double> N_limits_R,
-                        std::vector<double> N_k_R,
-                        std::vector<double> SWC_k_R) {
-  
-  // TODO: make the N_available link to the uptake equations that I have made!
+double Microbe_Uptake(double C_microbe,
+                      double N_micorbe,
+                      double NC_microbe_opt,
+                      double N_avaliable,
+                      double T,
+                      double SWC,
+                      std::vector<double> N_in_soil_R,
+                      std::vector<double> N_limits_R,
+                      std::vector<double> N_k_R,
+                      std::vector<double> SWC_k_R) {
+
+  /*
+   * Nitrogen limitation // TODO: remeber mass dependance
+   */
   
   N_balence N_in_soil = vector_to_N_balence(N_in_soil_R);
   N_balence N_limits = vector_to_N_balence(N_limits_R);
   N_balence N_k = vector_to_N_balence(N_k_R);
   N_balence SWC_k = vector_to_N_balence(SWC_k_R);
   
-  
   double NC_in_micorbe = N_micorbe/C_microbe;
   double N_micorbe_uptake = (uptake_organic_N(N_in_soil.Norg, T, N_limits.Norg, N_k.Norg, SWC, SWC_k.Norg) + 
                             uptake_NH4(N_in_soil.NH4, T, N_limits.NH4, N_k.NH4, SWC, SWC_k.NH4) + 
                             uptake_NO3(N_in_soil.NO3, T, N_limits.NO3, N_k.NO3, SWC, SWC_k.NO3))*
                             N_avaliable*
-                            (1 - (NC_in_micorbe)/(NC_microbe_opt));
+                            (1 - (NC_in_micorbe)/(NC_microbe_opt)); 
   
-  return(N_micorbe_uptake);
+  /*
+   * Carbon limitation
+   */
+  
+  double C_microbe_uptake = uptake_C(N_in_soil.C, T, N_limits.C, N_k.C, SWC, SWC_k.C);
+  
+/*
+   * Uptake
+   */
+  
+  double out = std::max(N_micorbe_uptake, C_microbe_uptake);
+  
+  return(out);
 }
 
 
