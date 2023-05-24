366bf8b3e24c8928c024a709489dc9636724aac4
Joanna <joanna.x.simms@helsinki.fi>
2023-05-24 17:54:13 +0300

    balence of C and N after microbes updated, as well as the arguments to the functions.

--- src/symphony_many_FOM.cpp
+++ src/symphony_many_FOM.cpp
@@ -17,12 +17,12 @@ Rcpp::List symphony_multiple_FOM_daily(std::vector<double> params, // TODO: need
                                        double Litter_woody, // TODO: these should come from CASSIA
                                        double Litter_roots, // TODO: these should come from CASSIA
                                        double Litter_mycelium, // TODO: should come from Mycofon / gorwth
-                                       double NH4_avaliable_in, // TODO: this should be from the last iteration
-                                       double NO3_avaliable_in,
-                                       double Norg_avaliable_in,
-                                       double NH4_used_in, // TODO: this should be from the last iteration
-                                       double NO3_used_in, // TODO: this should be from the last iteration
-                                       double Norg_used_in, // TODO: this should be from the last iteration
+                                       double NH4, // TODO: this should be from the last iteration
+                                       double NO3,
+                                       double NH4_used, // TODO: this should be from the last iteration
+                                       double NO3_used, // TODO: this should be from the last iteration
+                                       double FOM_Norg_used, // TODO: this should be from the last iteration
+                                       double SOM_Norg_used, // TODO: this should be from the last iteration
                                        std::vector<double> respiration_microbes_params,
                                        std::vector<double> N_in_soil_R,
                                        std::vector<double> N_limits_R,
@@ -42,57 +42,54 @@ Rcpp::List symphony_multiple_FOM_daily(std::vector<double> params, // TODO: need
   symphony_parameters symphony_params = vector_to_symphony(params);
   // alpha and beta should be more precise and the same as the parameters in the microbe uptake 
   
-  // DECLAIRATIONS
-  double NH4_avaliable_out;
-  double NO3_avaliable_out;
-  double Norg_avaliable_out;
+  // SOIL INITIALISATION
+  double C_FOM = C_FOM_needles + C_FOM_woody + C_FOM_roots + C_FOM_mycelium;
   
   /*
    * Nitrogen processes
    */
   
-  // N in
-  // TODO: check that the N things are correct in terms of in and out, as well as types and units!
-  NH4_avaliable_out = NH4_avaliable_in - NH4_used_in;
-  NO3_avaliable_out = NO3_avaliable_in - NO3_used_in;
-  Norg_avaliable_out = Norg_avaliable_in - Norg_used_in;
+  // STEP 1: Set the values, considering the other models
+  // TODO: check that the N things are correct in terms of in and out - does this calculation really need to be here? Units!
+  NH4 = NH4 - NH4_used;
+  NO3 = NO3 - NO3_used;
+  C_FOM = C_FOM - FOM_Norg_used;
+  C_SOM = C_SOM - SOM_Norg_used;
   
-  // N immobilisation, mineralisation
-  // This is linked to the uptake in the Microbe upatek function, where the C limitation is considered against the n limitation of avaliablity, now simply multiply by the mass
-  // Assume that the microbes are a group that can take up all of the N types fairly evenly
-  // TODO: currently assuming that the decomposition isn't FOM type based, although the types do exist later in the code
-  double NH4_micorbe_uptake_FOM = Microbe_Uptake(C_decompose_FOM, CN_microbe*C_decompose_FOM, NC_microbe_opt, NH4_avaliable_out, NO3_avaliable_out, Norg_avaliable_out, Tmb, SWC, N_in_soil_R, N_limits_R, N_k_R, SWC_k_R)[1];
-  double NO3_micorbe_uptake_FOM = Microbe_Uptake(C_decompose_FOM, CN_microbe*C_decompose_FOM, NC_microbe_opt, NH4_avaliable_out, NO3_avaliable_out, Norg_avaliable_out, Tmb, SWC, N_in_soil_R, N_limits_R, N_k_R, SWC_k_R)[2];
-  double Norg_micorbe_uptake_FOM = Microbe_Uptake(C_decompose_FOM, CN_microbe*C_decompose_FOM, NC_microbe_opt, NH4_avaliable_out, NO3_avaliable_out, Norg_avaliable_out, Tmb, SWC, N_in_soil_R, N_limits_R, N_k_R, SWC_k_R)[3];
+  // STEP 2: consider the uptake functions, for FOM and SOM, 
+      // this includes the decomposition and mineralisation / immobilisation
+      // Apart from the biomass that the microbes specialise in, the second N uptake is considered to be equally driven between the types
+      
+  // FOM
+  Rcpp::List FOM_after_microbe_activity_list = Microbe_Uptake(C_decompose_FOM, CN_microbe*C_decompose_FOM, NC_microbe_opt, NH4, NO3, C_FOM, Tmb, SWC, N_limits_R, N_k_R, SWC_k_R, false, C_FOM);
+  N_balence FOM_after_microbe_activity = list_to_N_balence(FOM_after_microbe_activity_list);
+  // SOM
+  Rcpp::List SOM_after_microbe_activity_list = Microbe_Uptake(C_decompose_SOM, CN_microbe*C_decompose_SOM, NC_microbe_opt, NH4, NO3, C_SOM, Tmb, SWC, N_limits_R, N_k_R, SWC_k_R, true, C_FOM);
+  N_balence SOM_after_microbe_activity = list_to_N_balence(SOM_after_microbe_activity_list);
   
-  NH4_avaliable_out = NH4_avaliable_out - C_decompose_FOM*NH4_micorbe_uptake_FOM; // TODO: check units
-  NO3_avaliable_out = NO3_avaliable_out - C_decompose_FOM*NO3_micorbe_uptake_FOM;
-  Norg_avaliable_out = Norg_avaliable_out - C_decompose_FOM*Norg_micorbe_uptake_FOM;
+  // Update pure inorganic pools
+  NH4 = NH4 - C_decompose_FOM*FOM_after_microbe_activity.NH4 - C_decompose_SOM*SOM_after_microbe_activity.NH4; // TODO: check units
+  NO3 = NO3 - C_decompose_FOM*FOM_after_microbe_activity.NO3 - C_decompose_SOM*SOM_after_microbe_activity.NO3;
   
   /*
    * Carbon processes
    */
-  // Decomposition / uptake
-  double C_micorbe_uptake_FOM = Microbe_Uptake(C_decompose_FOM, CN_microbe*C_decompose_FOM, NC_microbe_opt, NH4_avaliable_out, NO3_avaliable_out, Norg_avaliable_out, Tmb, SWC, N_in_soil_R, N_limits_R, N_k_R, SWC_k_R)[4];
-  double C_micorbe_uptake_SOM = Microbe_Uptake(C_decompose_SOM, CN_microbe*C_decompose_SOM, NC_microbe_opt, NH4_avaliable_out, NO3_avaliable_out, Norg_avaliable_out, Tmb, SWC, N_in_soil_R, N_limits_R, N_k_R, SWC_k_R)[4];
+  // STEP 1: Update the FOM mass
+  // TODO: SOM part should be in the right units! no multiplier currently
+  double total_decomposition = (C_decompose_FOM*FOM_after_microbe_activity.C + C_decompose_SOM*SOM_after_microbe_activity.Norg_FOM);
+  // the nitrogen transfer should be by N and litter type
+  C_FOM_needles = C_FOM_needles + Litter_needles - (C_FOM_needles/C_FOM)*total_decomposition;
+  C_FOM_woody = C_FOM_woody + Litter_woody - (C_FOM_woody/C_FOM)*total_decomposition;
+  C_FOM_roots = C_FOM_roots + Litter_roots - (C_FOM_roots/C_FOM)*total_decomposition;
+  C_FOM_mycelium = C_FOM_mycelium + Litter_mycelium - (C_FOM_mycelium/C_FOM)*total_decomposition;
   
+  // STEP 2: Update the SOM mass
+  C_SOM = C_SOM - C_decompose_SOM*SOM_after_microbe_activity.C + microbe_turnover*(C_decompose_FOM + C_decompose_SOM);
   
-  // Carbon balance calculations
-  double total_FOM_before = C_FOM_needles + C_FOM_woody + C_FOM_roots + C_FOM_mycelium;
+  // STEP 3: Update the microbes
+  C_decompose_FOM = (1 + C_decompose_FOM*FOM_after_microbe_activity.C - respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]) - microbe_turnover) * C_decompose_FOM;
   
-  // Update the FOM mass
-  C_FOM_needles = C_FOM_needles + Litter_needles - (C_FOM_needles/total_FOM_before)*C_decompose_FOM*C_micorbe_uptake_FOM; // the nitrogen transfer should be by N and litter type
-  C_FOM_woody = C_FOM_woody + Litter_woody - (C_FOM_woody/total_FOM_before)*C_decompose_FOM*C_micorbe_uptake_FOM;
-  C_FOM_roots = C_FOM_roots + Litter_roots - (C_FOM_roots/total_FOM_before)*C_decompose_FOM*C_micorbe_uptake_FOM;
-  C_FOM_mycelium = C_FOM_needles + Litter_mycelium - (C_FOM_mycelium/total_FOM_before)*C_decompose_FOM*C_micorbe_uptake_FOM;
-  
-  // Update the SOM mass
-  C_SOM = C_SOM + microbe_turnover*(C_decompose_FOM + C_decompose_SOM); // TODO: doesn't have the emergency turnover any more - in uptake?
-  
-  // Update the microbes
-  C_decompose_FOM = (1 + C_micorbe_uptake_FOM - respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]) - microbe_turnover) * C_decompose_FOM;
-  
-  C_decompose_SOM = (1 - C_micorbe_uptake_SOM - respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]) - microbe_turnover) * C_decompose_SOM;
+  C_decompose_SOM = (1 + C_decompose_SOM*SOM_after_microbe_activity.C - respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]) - microbe_turnover) * C_decompose_SOM;
   
   // Output
   return Rcpp::List::create(Rcpp::_["C_decompose_FOM"] = C_decompose_FOM,
@@ -102,9 +99,8 @@ Rcpp::List symphony_multiple_FOM_daily(std::vector<double> params, // TODO: need
                             Rcpp::_["C_FOM_roots"] = C_FOM_roots,
                             Rcpp::_["C_FOM_mycelium"] = C_FOM_mycelium,
                             Rcpp::_["C_SOM"] = C_SOM,
-                            Rcpp::_["NH4"] = NH4_avaliable_out,
-                            Rcpp::_["NO3"] = NO3_avaliable_out,
-                            Rcpp::_["Norg"] = Norg_avaliable_out,
+                            Rcpp::_["NH4"] = NH4,
+                            Rcpp::_["NO3"] = NO3,
                             Rcpp::_["Microbe_respiration"] = respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]));
 }
 
