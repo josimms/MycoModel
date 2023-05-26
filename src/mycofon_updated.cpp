24430e11c03fe8bb9ffba69d4028ad362c0403d6
Joanna <joanna.x.simms@helsinki.fi>
2023-05-26 17:15:57 +0300

    Inputs updated to work with the other uptake models

--- src/mycofon_updated.cpp
+++ src/mycofon_updated.cpp
@@ -23,7 +23,9 @@ Rcpp::List mycofon_balence(double Allocation_C_CASSIA,
                            double C_allocation,
                            double N_allocation,
                            std::vector<double> N_in_root_R,
-                           std::vector<double> N_in_soil_R,
+                           double NH4,
+                           double NO3,
+                           double FOM_Norg,
                            double C_in_root,
                            double NC_in_root_opt,
                            double T,
@@ -35,12 +37,13 @@ Rcpp::List mycofon_balence(double Allocation_C_CASSIA,
                            double NC_fungal_opt,
                            double mantle_mass,
                            double ERM_mass,
-                           double N_avaliable){
+                           double N_avaliable,
+                           std::vector<double> parameters_NH4_on_NO3) {
   
   // Mycorrhizal rate
   double m = fungal_mass / (root_mass * optimal_root_fungal_biomass_ratio);
   if (m > 1) {
-    std::cout << "Warning: The mycorhization value (m) is more than 1 - check the fungal_mass, root_mass and optimal_root_fungal_biomass_ratio values"; 
+    std::cout << "Warning:\nThe mycorhization value (m) is more than 1 - check the fungal_mass, root_mass and optimal_root_fungal_biomass_ratio values"; 
   }
   // dC^r/dt TODO: this need to be linked with the CASSIA C sections
   C_roots = C_roots + Allocation_C_CASSIA - (1 - m)*C_roots*turnover_roots - m*C_roots*turnover_fungal - respiration(Tsb, a_roots, b_roots)*C_roots - C_allocation;
@@ -51,47 +54,47 @@ Rcpp::List mycofon_balence(double Allocation_C_CASSIA,
   // TODO: should the N in the roots in different forms be differentiated - or can it be grouped at this stage?
   double root_NC_ratio = N_roots / C_roots;
   double fungal_NC_ratio = N_fungal / C_fungal;
+  
+  
   //dN^r/dt
-  N_roots = N_roots + 
-    N_allocation + 
-    Plant_N_Uptake(C_in_root, 
-                   NC_in_root_opt, 
-                   T,
-                   SWC,
-                   m,
-                   N_in_root_R, 
-                   N_in_soil_R,
-                   N_limits_R,
-                   N_k_R,
-                   SWC_k_R,
-                   C_roots,
-                   C_roots_biomass,
-                   N_avaliable,
-                   N_allocation) - 
+  double uptake_plant = Plant_N_Uptake(NC_in_root_opt, 
+                                       T,
+                                       SWC,
+                                       m,
+                                       NH4,    // TODO: make this have in input that works with the output of the soil function
+                                       NO3,
+                                       FOM_Norg,
+                                       N_limits_R,
+                                       N_k_R,
+                                       SWC_k_R,
+                                       C_roots,
+                                       N_roots,
+                                       C_roots_biomass,
+                                       N_allocation,
+                                       parameters_NH4_on_NO3)[1];
+
+  N_roots = N_roots + N_allocation + uptake_plant - 
                      turnover_roots*C_roots*root_NC_ratio - 
                      N_to_CASSIA;
   
   //dN^f/dt
-  N_fungal = N_fungal + 
-    Fungal_N_Uptake(C_roots,
-                    N_roots,
-                    C_fungal,
-                    N_fungal,
-                    NC_fungal_opt,
-                    mantle_mass,
-                    ERM_mass,
-                    C_roots_biomass,
-                    N_avaliable,
-                    N_to_CASSIA,
-                    T,
-                    SWC,
-                    N_in_soil_R,
-                    N_limits_R,
-                    N_k_R,
-                    SWC_k_R) - 
-                      turnover_fungal*C_fungal*fungal_NC_ratio - 
-                      N_allocation;
+  double uptake_fungal = Fungal_N_Uptake(C_fungal,
+                                         N_fungal,
+                                         NC_fungal_opt,
+                                         mantle_mass,
+                                         ERM_mass,
+                                         C_roots_biomass,
+                                         T,
+                                         SWC,
+                                         NH4,
+                                         NO3,
+                                         FOM_Norg,
+                                         N_limits_R,
+                                         N_k_R,
+                                         SWC_k_R)[1];
   
+  N_fungal = N_fungal + uptake_fungal - turnover_fungal*C_fungal*fungal_NC_ratio - N_allocation;
+
   return Rcpp::List::create(Rcpp::_["C_roots"] = C_roots,
                             Rcpp::_["C_fungal"] = C_fungal,
                             Rcpp::_["N_roots"] = N_roots,
