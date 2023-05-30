010d1c93d93bbccbdccee364562b0f361f962f4d
Joanna <joanna.x.simms@helsinki.fi>
2023-05-30 15:50:15 +0300

    Decision functions updated to have more outputs

--- src/decision_functions.cpp
+++ src/decision_functions.cpp
@@ -1,25 +1,35 @@
+#include "mycomodel.h"
+
 // [[Rcpp::export]]
-double plant_decision(double C_roots,
-                      double N_roots,
-                      double NC_in_root_opt) {
-  double out;
-  if (C_roots/N_roots > NC_in_root_opt) {
-    out = 1;
-  } else {
-    out = 0;
-  }
-  return(out);
+Rcpp::List plant_decision(double C_roots,
+                          double N_roots,
+                          double NC_in_root_opt) {
+    double out;
+    if (C_roots/N_roots > NC_in_root_opt) {
+      out = 1;
+    } else {
+      out = 0;
+    }
+    
+    double demand = 1;
+    
+    return(Rcpp::List::create(Rcpp::_["demand"] = demand,
+                              Rcpp::_["C_given"] = out));
 }
 
 // [[Rcpp::export]]
-double myco_decision(double C_fungal,
-                     double N_fungal,
-                     double NC_in_fungal_opt) {
+Rcpp::List myco_decision(double C_fungal,
+                         double N_fungal,
+                         double NC_in_fungal_opt) {
   double out;
   if (C_fungal/N_fungal > NC_in_fungal_opt) {
     out = 1;
   } else {
     out = 0;
   }
-  return(out);
+  
+  double demand = 1;
+  
+  return(Rcpp::List::create(Rcpp::_["demand"] = demand,
+                            Rcpp::_["N_given"] = out));
 }
