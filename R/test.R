bacd81fc0879e7863a3d839986c90e65cefcb55c
Joanna <joanna.x.simms@helsinki.fi>
2023-06-06 15:52:05 +0300

    Test file created and test graph functions added to the file

---
+++ R/test.R
@@ -0,0 +1,26 @@
+tests <- function() {
+  ###
+  # Check that the outputs are providing the right behaviour
+  ### 
+  
+  respiration_graphs()
+  
+  nitrogen_graphs()
+  
+  decision_graphs()
+  
+  myco_growth_graphs()
+  
+  nitrogen_graphs_plant()
+  nitrogen_graphs_fungal()
+  nitrogen_graphs_microbe()
+  
+  mycofon_balence_graphs()
+  
+  ###
+  # Check that there is mass conservation
+  ###
+  
+  
+  
+}
