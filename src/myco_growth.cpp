fe2afdea693b6a55295ff56ec37a76e89fea6929
Joanna <joanna.x.simms@helsinki.fi>
2023-06-16 13:57:55 +0300

    _fungal changed to _fungal_in = this means that there is less confusion between the state variables

--- src/myco_growth.cpp
+++ src/myco_growth.cpp
@@ -1,21 +1,21 @@
 #include "mycomodel.h"
 
 // [[Rcpp::export]]
-Rcpp::List myco_growth(double C_fungal,
-                       double N_fungal,
+Rcpp::List myco_growth(double C_fungal_in,
+                       double N_fungal_in,
                        double a,
                        double b)
 {
   
   double carbon_used;
   double nitrogen_used;
-  double out = std::min(a*C_fungal, b*N_fungal); // The growth is just a experiential function, limited by the element
-  if (out == a*C_fungal) {
-    carbon_used = a*C_fungal;
-    nitrogen_used = b*N_fungal*(a*C_fungal)/(b*N_fungal);
-  } else if (out == b*N_fungal) {
-    carbon_used = a*C_fungal*(b*N_fungal)/(a*C_fungal);
-    nitrogen_used = b*N_fungal;
+  double out = std::min(a*C_fungal_in, b*N_fungal_in); // The growth is just a experiential function, limited by the element
+  if (out == a*C_fungal_in) {
+    carbon_used = a*C_fungal_in;
+    nitrogen_used = b*N_fungal_in*(a*C_fungal_in)/(b*N_fungal_in);
+  } else if (out == b*N_fungal_in) {
+    carbon_used = a*C_fungal_in*(b*N_fungal_in)/(a*C_fungal_in);
+    nitrogen_used = b*N_fungal_in;
   }
   
   return Rcpp::List::create(Rcpp::_["growth"] = out,
