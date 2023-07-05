aee4a802cdacec1da5770053658c4b977d0e34b3
Joanna <joanna.x.simms@helsinki.fi>
2023-07-05 17:38:34 +0300

    the function was changed so that there cannot be negative growth

--- src/myco_growth.cpp
+++ src/myco_growth.cpp
@@ -1,24 +1,25 @@
 #include "mycomodel.h"
 
 // [[Rcpp::export]]
-Rcpp::List myco_growth(double C_fungal_in,
-                       double N_fungal_in,
+Rcpp::List myco_growth(double C_fungal, // per biomass
+                       double N_fungal, // per biomass
                        double a,
                        double b)
 {
   
   double carbon_used;
   double nitrogen_used;
-  double out = std::min(a*C_fungal_in, b*N_fungal_in); // The growth is just a experiential function, limited by the element
-  if (out == a*C_fungal_in) {
-    carbon_used = a*C_fungal_in;
-    nitrogen_used = b*N_fungal_in*(a*C_fungal_in)/(b*N_fungal_in);
-  } else if (out == b*N_fungal_in) {
-    carbon_used = a*C_fungal_in*(b*N_fungal_in)/(a*C_fungal_in);
-    nitrogen_used = b*N_fungal_in;
+  double out_1 = std::min(a*C_fungal, b*N_fungal); // The growth is just a experiential function, limited by the element
+  double out_2 = std::max(out_1, 0.0); // No negative growth
+  if (out_2 == a*C_fungal) {
+    carbon_used = a*C_fungal;
+    nitrogen_used = b*N_fungal*(a*C_fungal)/(b*N_fungal);
+  } else if (out_2 == b*N_fungal) {
+    carbon_used = a*C_fungal*(b*N_fungal)/(a*C_fungal);
+    nitrogen_used = b*N_fungal;
   }
   
-  return Rcpp::List::create(Rcpp::_["growth"] = out,
+  return Rcpp::List::create(Rcpp::_["growth"] = out_2,
                             Rcpp::_["carbon_used"] = carbon_used,
                             Rcpp::_["nitrogen_used"] = nitrogen_used);
 }
