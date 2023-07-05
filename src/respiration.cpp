47139df637a1194e7a7239556a444960a2b0c04e
Joanna <joanna.x.simms@helsinki.fi>
2023-07-05 17:32:58 +0300

    Respiration function updated so the condition includes out not respiration

--- src/respiration.cpp
+++ src/respiration.cpp
@@ -6,13 +6,13 @@ double respiration(double Tmb, double Rm, double Q10)
   /*
    * CASSIA FUNCTION FOR ROOT RESPIRATION
    */
-   double respiration = Rm * (exp(Q10/10 * Tmb) - exp(-log(Q10) / 2));
+   double respiration = Rm * exp(Q10/10 * Tmb) - exp(-log(Q10)/2);
    
    double out;
-   if (out <= 0) {
-     out = 0;
-   } else {
+   if (respiration > 0) {
      out = respiration;
+   } else {
+     out = 0;
    }
   
   return(out);
