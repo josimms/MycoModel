5cf14d8e672bc5fb80bef672f0c117ceb3f18d04
Joanna <joanna.x.simms@helsinki.fi>
2023-07-05 17:39:05 +0300

    Stratergies updated to make them the same as in Franklin 2014 and Mycofon

--- src/decision_functions.cpp
+++ src/decision_functions.cpp
@@ -3,57 +3,89 @@
 // [[Rcpp::export]]
 Rcpp::List plant_decision(double C_roots,
                           double N_roots,
-                          double NC_in_root_opt,
-                          double C_value_param,
-                          double N_value_param) {
-  
-    double out;
-    if (C_roots/N_roots > NC_in_root_opt) {
-      out = 1;
+                          double C_fungal,
+                          double optimal_root_funga_biomass_ratio,
+                          double N_allo,
+                          double max_C_allocation_CASSIA) {
+    
+    /*
+     * Carbon Allocation as in Mycofon!
+     * 
+     * N_roots should be the N availability in the orginal model!
+     * Thus the condition is changed as well the condition was original
+     * N_av > 0.01 kg - N kg soil-1
+     * and
+     * N_allo < 0.5 (N_uptake_root + N_allo)
+     * also
+     * allomax * photosynthesis
+     */
+    
+    double allomax;
+    if (N_roots < 0.01) { // TODO: consider this again
+      allomax = 1 - (1 - pow(exp(-50*N_roots), 3));
     } else {
-      out = 0;
+      allomax = 0.2;
     }
     
-    double NC_in_root = N_roots/C_roots;
-    double demand = 1 - NC_in_root/NC_in_root_opt;
-    if (demand < 0) {
-      std::cout << "Warning:\nDemand is negative, rethink optimal root value (NC_in_root_opt) value.\n";
-    };
+    double allo; // Allocates all if it can meet it's own demand
+    if (N_allo < 0.5*N_roots) { // TODO: consider this again
+      allo = N_allo / (N_roots + N_allo);
+    } else {
+      allo = 1; 
+    }
     
-    double C_value = C_value_param*C_roots;
-    double N_value = N_value_param*N_roots;
+    double C_allo = std::max(std::min(allomax * C_roots, allo * (C_roots * optimal_root_funga_biomass_ratio) - C_fungal), 0.0);
+      
+    /*
+     *  Carbon Allocation in Franklin 2014
+     *  
+     *  C_f is defined within a set of equations, but basically the excess after growth and turnover
+     *  
+     *  C_f = Photosynthesis - G_p / y_p - turnover
+     */
     
-    return(Rcpp::List::create(Rcpp::_["demand"] = demand,
-                              Rcpp::_["C_value"] = C_value,
-                              Rcpp::_["N_value"] = N_value,
-                              Rcpp::_["N_given"] = out));
+    double C_f = std::max(max_C_allocation_CASSIA, 0.0); // Value from CASSIA
+    
+    return(Rcpp::List::create(Rcpp::_["Mycofon_demand"] = 1,
+                              Rcpp::_["Mycofon_allocation"] = C_allo,
+                              Rcpp::_["Franklin_demand"] = 1,
+                              Rcpp::_["Franklin_allocation"] = C_f));
 }
 
 // [[Rcpp::export]]
 Rcpp::List myco_decision(double C_fungal,
                          double N_fungal,
+                         double C_roots,
+                         double N_roots,
                          double NC_fungal_opt,
-                         double mantle_mass,
-                         double ERM_mass,
-                         double percentage_C_biomass,
-                         double C_value_param,
-                         double N_value_param) {
+                         double growth_C,
+                         double growth_N) {
   
-  double out;
-  if (C_fungal/N_fungal > NC_fungal_opt) {
-    out = 1;
-  } else {
-    out = 0;
-  }
+  /* 
+   * Nitrogen allocation from Mycofon!
+   * 
+   * N_allo = N_max (1 - NC_root / NC_rootopt)
+   * 
+   * N_max should be uptake rather than biomass! 
+   */
   
-  double NC_in_fungal = C_fungal/N_fungal;
-  double demand = (C_fungal/percentage_C_biomass)*(mantle_mass/ERM_mass)*(1 - NC_in_fungal/NC_fungal_opt);
+  double N_allo = std::max(N_fungal*(1 - (N_roots / C_roots) / NC_fungal_opt), 0.0);
   
-  double C_value = C_value_param*C_fungal;
-  double N_value = N_value_param*N_fungal;
+  /*
+   * Nitrogen allocation from Franklin 2014
+   * 
+   * N_p = Uptake(B) - y_f C_f N:C
+   * 
+   * Where the Uptake(B) is the biomass which is devoted to uptake
+   *    TODO: mantle / ERM divide could be used here!
+   *    TODO: Needs to be uptake rather than biomass though!
+   */
   
-  return(Rcpp::List::create(Rcpp::_["demand"] = demand,
-                            Rcpp::_["C_value"] = C_value,
-                            Rcpp::_["N_value"] = N_value,
-                            Rcpp::_["N_given"] = out));
+  double growth = myco_growth(C_fungal, N_fungal, growth_C, growth_N)[2]; // Nitrogen used output
+  double N_p = std::max(N_fungal - growth, 0.0);
+  
+  return(Rcpp::List::create(Rcpp::_["Mycofon_demand"] = 1,
+                            Rcpp::_["Mycofon_allocation"] = N_allo,
+                            Rcpp::_["Franklin_demand"] = 1,
+                            Rcpp::_["Franklin_allocation"] = N_p));
 }
