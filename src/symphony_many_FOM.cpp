857d69758f42525fd7c1bed7cf0e5cb1d5533ad1
Simms Joanna <josimms@lx3-800-29412.ad.helsinki.fi>
2023-11-27 12:06:39 +0200

    Arguments updated

--- src/symphony_many_FOM.cpp
+++ src/symphony_many_FOM.cpp
@@ -3,13 +3,13 @@
 // [[Rcpp::export]]
 Rcpp::List symphony_multiple_FOM_daily(double Tmb,
                                        double SWC,
-                                       double C_FOM_needles,
-                                       double C_FOM_woody,
-                                       double C_FOM_roots,
-                                       double C_FOM_mantle,
-                                       double C_FOM_ERM,
-                                       double C_SOM,
-                                       double N_SOM,
+                                       double C_FOM_needles_old,
+                                       double C_FOM_woody_old,
+                                       double C_FOM_roots_old,
+                                       double C_FOM_mantle_old,
+                                       double C_FOM_ERM_old,
+                                       double C_SOM_old,
+                                       double N_SOM_old,
                                        double C_decompose_FOM,
                                        double C_decompose_SOM,
                                        double N_decompose_FOM,
@@ -21,8 +21,8 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
                                        double Litter_ERM,
                                        double imobilisation,
                                        double assimilation,
-                                       double NH4,
-                                       double NO3,
+                                       double NH4_old,
+                                       double NO3_old,
                                        double NC_needles,
                                        double NC_woody,
                                        double NC_roots,
@@ -47,15 +47,16 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
    * Initialisation or declaration
    */
   
+  
   // INITILISATION FROM VECTORS
   // Parameters into symphony_parameters format
   
   // SOIL INITIALISATION NEW INPUT!
-  C_FOM_needles = C_FOM_needles + Litter_needles;             // C kg
-  C_FOM_woody = C_FOM_woody + Litter_woody;                   // C kg
-  C_FOM_roots = C_FOM_roots + Litter_roots;                   // C kg
-  C_FOM_mantle = C_FOM_mantle + Litter_mantle;                // C kg
-  C_FOM_ERM = C_FOM_ERM + Litter_ERM;                         // C kg
+  double C_FOM_needles = C_FOM_needles_old + Litter_needles;             // C kg
+  double C_FOM_woody = C_FOM_woody_old + Litter_woody;                   // C kg
+  double C_FOM_roots = C_FOM_roots_old + Litter_roots;                   // C kg
+  double C_FOM_mantle = C_FOM_mantle_old + Litter_mantle;                // C kg
+  double C_FOM_ERM = C_FOM_ERM_old + Litter_ERM;                         // C kg
   
   // AGGREGATION
   double C_FOM = C_FOM_needles +
@@ -69,15 +70,17 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
     C_FOM_mantle * NC_mantle + 
     C_FOM_ERM * NC_ERM; // C kg
 
+   
   /*
    * Nitrogen processes
    */
+
   
   // STEP 1: Set the values, considering the other models
-  NH4 = NH4 - NH4_used_Plant - NH4_used_Fungal;             // C kg
-  NO3 = NO3 - NO3_used_Plant - NO3_used_Fungal;             // C kg
+  double NH4 = NH4_old - NH4_used_Plant - NH4_used_Fungal;             // C kg
+  double NO3 = NO3_old - NO3_used_Plant - NO3_used_Fungal;             // C kg
   N_FOM = N_FOM - FOM_Norg_used_Plant - FOM_Norg_used_Fungal;    // C kg
-  N_SOM = N_SOM;                                            // C kg
+  double N_SOM = N_SOM_old;                                            // C kg
   
   double NC_Litter = N_FOM/C_FOM;
   
@@ -87,11 +90,12 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
   // this includes the decomposition and mineralisation / immobilisation
   // Apart from the biomass that the microbes specialise in, the second N uptake is considered to be equally driven between the types
   
+  
   // FOM
   Rcpp::List FOM_after_microbe_activity_list = Microbe_Uptake(C_decompose_FOM, N_decompose_FOM, C_FOM, NC_microbe_opt, NH4, NO3, N_FOM, Tmb, SWC, NC_Litter, imobilisation, assimilation, N_limits_R, N_k_R, SWC_k_R, false, respiration_microbes_params);
   N_balence FOM_after_microbe_activity = list_to_N_balence(FOM_after_microbe_activity_list);    // C kg
   // SOM
-  Rcpp::List SOM_after_microbe_activity_list = Microbe_Uptake(C_decompose_SOM, N_decompose_SOM, C_SOM, NC_microbe_opt, NH4, NO3, N_FOM, Tmb, SWC, NC_Litter, imobilisation, assimilation, N_limits_R, N_k_R, SWC_k_R, true, respiration_microbes_params);
+  Rcpp::List SOM_after_microbe_activity_list = Microbe_Uptake(C_decompose_SOM, N_decompose_SOM, C_SOM_old, NC_microbe_opt, NH4, NO3, N_FOM, Tmb, SWC, NC_Litter, imobilisation, assimilation, N_limits_R, N_k_R, SWC_k_R, true, respiration_microbes_params);
   N_balence SOM_after_microbe_activity = list_to_N_balence(SOM_after_microbe_activity_list);    // C kg
   
   // Update pure inorganic pools
@@ -101,6 +105,7 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
   N_FOM = N_FOM - C_decompose_FOM*FOM_after_microbe_activity.Norg - C_decompose_SOM*SOM_after_microbe_activity.Norg_FOM;    // C kg eq
   N_SOM = N_SOM - C_decompose_SOM*SOM_after_microbe_activity.Norg;    // C kg eq 
 
+  
   /*
    * Carbon processes
    */
@@ -115,7 +120,7 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
   C_FOM_ERM = C_FOM_ERM - (C_FOM_ERM/C_FOM)*total_decomposition;                        // C kg
   
   // STEP 2: Update the SOM mass
-  C_SOM = C_SOM - C_decompose_SOM*SOM_after_microbe_activity.C + microbe_turnover*(C_decompose_FOM + C_decompose_SOM);   // C kg
+  double C_SOM = C_SOM_old - C_decompose_SOM*SOM_after_microbe_activity.C + microbe_turnover*(C_decompose_FOM + C_decompose_SOM);   // C kg
   
   // STEP 3: Update the microbes
   C_decompose_FOM = microbe_turnover + (1 + 
@@ -126,6 +131,10 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
     SOM_after_microbe_activity.C - 
     respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2])) * C_decompose_SOM;   // C kg
   
+  double amino_acids = 1; // TOOD: add the organic S storage into the model
+  
+  
+
   // Output
   return Rcpp::List::create(Rcpp::_["C_decompose_FOM"] = C_decompose_FOM,          // C kg
                             Rcpp::_["C_decompose_SOM"] = C_decompose_SOM,          // C kg
@@ -134,10 +143,14 @@ Rcpp::List symphony_multiple_FOM_daily(double Tmb,
                             Rcpp::_["C_FOM_roots"] = C_FOM_roots,                  // C kg
                             Rcpp::_["C_FOM_mantle"] = C_FOM_mantle,                // C kg
                             Rcpp::_["C_FOM_ERM"] = C_FOM_ERM,                      // C kg
+                            Rcpp::_["N_FOM"] = N_FOM,                              // C kg
                             Rcpp::_["C_SOM"] = C_SOM,                              // C kg
+                            Rcpp::_["N_SOM"] = N_SOM,
                             Rcpp::_["NH4"] = NH4,                                  // C kg
                             Rcpp::_["NO3"] = NO3,                                  // C kg
                             Rcpp::_["Microbe_respiration"] = respiration(Tmb, respiration_microbes_params[1], respiration_microbes_params[2]));    // C kg
+  
+  return 0;
 }
 
 
